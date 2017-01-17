ae05d21022b06c59cc92bc8450d665213fc9c47d
holmfridurge <holmfridurge@gmail.com>
2017-01-17 21:29:32 +0000

    grunt and package

--- Gruntfile.js
+++ Gruntfile.js
@@ -250,7 +250,7 @@ module.exports = function (grunt) {
           html: {
             steps: {
               js: ['concat', 'uglifyjs'],
-              css: ['cssmin']
+              //css: ['cssmin']
             },
             post: {}
           }
@@ -472,7 +472,7 @@ module.exports = function (grunt) {
     'ngAnnotate',
     'copy:dist',
     'cdnify',
-    'cssmin',
+    //'cssmin',
     'uglify',
     'filerev',
     'usemin',
